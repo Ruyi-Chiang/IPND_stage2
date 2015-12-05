9361a73bd9ccb947c55772ecfe772c930ff32bd2
Ruyi Chiang <ruyi.chiang@gmail.com>
2015-12-05 13:58:23 +0800

    refactors: Revise the code based on Udacity code review suggestions apart from line length

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -8,10 +8,10 @@ parts_of_lyrics_easy  = ["_1____", "_2___", "_3__", "_4__"]
 correct_answers_easy = ["spider", "spout", "rain", "sun"]
 
 
-#Medium quiz - Let it go
+# Medium quiz - Let it go
 medium_quiz = "Don't let them in, don't let them see. Be the good _1__ you always have to be. Conceal, don't feel, don't let them know. Well now they know.  Let it go, let it go. Can't hold it back _2_____. Let it go, let it go. Turn away and slam the door. I don't _3__ what they're going to say. Let the _4___ rage on. The cold never bothered me _5____"
 
-parts_of_lyrics_medium  = ["_1__", "_2_____", "_3__", "_4___", "_5____"]
+parts_of_lyrics_medium = ["_1__", "_2_____", "_3__", "_4___", "_5____"]
 
 correct_answers_medium = ["girl", "anymore", "care", "storm", "anyway"]
 
@@ -19,16 +19,16 @@ correct_answers_medium = ["girl", "anymore", "care", "storm", "anyway"]
 # Hard quiz - Hello by Adele
 hard_quiz = "Hello, it's _1. I was _2_______ if after all these years you'd like to meet. To go over everything. They say that time's supposed to heal ya. But I ain't done much _3_____. Hello, can you _4__ me. I'm in _5________ dreaming about who we used to be. When we were _6_____ and free. I've forgotten how it felt before the world fell at our feet"
 
-parts_of_lyrics_hard  = ["_1", "_2_______", "_3_____", "_4__", "_5________", "_6_____"]
+parts_of_lyrics_hard = ["_1", "_2_______", "_3_____", "_4__", "_5________", "_6_____"]
 
 correct_answers_hard = ["me", "wondering", "healing", "hear", "California", "younger"]
 
 
 # Level-choosing function: Ask user to choose quiz level
 def choose_level():
-	level = raw_input("Which level would you like to try? (easy/ medium/ hard)")
+	level = raw_input("Which level would you like to try? (easy/ medium/ hard) ")
 	while level not in ["easy", "medium", "hard"]:
-		level = raw_input("Please input easy/ medium/ hard. Which level?")
+		level = raw_input("Please input easy/ medium/ hard. Which level? ")
 	if level == "easy":
 		start_quiz(easy_quiz, parts_of_lyrics_easy, correct_answers_easy)
 	elif level == "medium":
@@ -42,12 +42,13 @@ def start_quiz(quiz, blanks, answers):
 	print quiz
 	num_blanks = len(blanks)
 	for quiz_num in range(0, num_blanks):
-		user_input= raw_input ("What's " + blanks[quiz_num] + "? ")
+		user_input = raw_input("What's " + blanks[quiz_num] + "? ")
 		while answer_incorrect(user_input, answers[quiz_num]):
-			user_input= raw_input ("oohoh.. incorrect. Try again. What's " + blanks[quiz_num] + "? ")
+			user_input = raw_input("oohoh.. incorrect. Try again. What's " + blanks[quiz_num] + "? ")
 		quiz = quiz.replace(blanks[quiz_num], answers[quiz_num])
 		print quiz
-		if quiz_num == num_blanks - 1: #num_blanks - 1 since Python is 0 based
+		#num_blanks - 1 since Python is 0 based
+		if quiz_num == num_blanks - 1: 
 			print "Congratulations! You finished the quiz."
 
 
